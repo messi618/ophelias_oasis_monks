9e9986a96dfad9cf6548acd620ebaa335a14550b
messi <vivekmessikumar@gmail.com>
2019-12-11 03:37:19 -0800

    Added Manager Login and  updated user login

--- scripts/loginMin.js
+++ scripts/loginMin.js
@@ -1,14 +1,4 @@
-$(function() {
-    $('#loginbutton').click(function(){
-        var uid=$('#uid').val();
-        var pass=$('#pass').val();
-        var u="vivekkumar@gmail.com"
-        var p="messi";
-          if(uid==u&&pass==p){
-            console.log("success");
-            window.location.href='http://google.com';
-          }
-          else
-          {window.alert('Authentication Failed');}
-    });
-  });
+sessionStorage.u="vivek@gmail.com"
+sessionStorage.p="messi";
+sessionStorage.mu="gleonard@gmail.com";
+sessionStorage.mp="messi";
