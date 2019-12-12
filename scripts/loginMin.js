014e77cff2b899a567a2cff7dd027aee89819946
messi <vivekmessikumar@gmail.com>
2019-12-12 01:15:55 -0800

    update booking and test login

--- scripts/loginMin.js
+++ scripts/loginMin.js
@@ -1,4 +1,6 @@
-sessionStorage.u="vivek@gmail.com"
-sessionStorage.p="messi";
-sessionStorage.mu="guyleonard@gmail.com";
-sessionStorage.mp="messi";
+//used for testing when we didnt have a DB
+
+//sessionStorage.u="vivek@gmail.com"
+//sessionStorage.p="messi";
+//sessionStorage.mu="guyleonard@gmail.com";
+//sessionStorage.mp="messi";
